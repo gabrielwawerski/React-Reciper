5989fd4281b91161da7cc512211bedbde90a1e84
4masm <gabriel.wawerski@gmail.com>
2020-05-24 22:05:26 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import data from './data/recipes.json';
+
+class Recipe extends React.Component {
+    render() {
+        return (
+            <div className="recipe">
+                <ImgWrapper {...this.props}/>
+                <RecipeInfo {...this.props}/>
+            </div>
+        );
+    }
+}
+
+class ImgWrapper extends React.Component {
+    render() {
+        return (
+            <div className="img-wrapper" style={{backgroundImage: `url(${this.props.src})`}}>
+                <div className="img-container"></div>
+                <input type="checkbox" className="checkbox" onClick={this.addRecipe}/>
+            </div>
+        );
+    }
+
+    addRecipe() {
+        console.log("clicked!")
+    }
+}
+
+class RecipeInfo extends React.Component {
+    render() {
+        return (
+            <div className="recipe-info">
+                <p className="recipe-title">{this.props.recipeTitle}</p>
+                <p className="recipe-description">{this.props.recipeDescription}</p>
+            </div>
+        );
+    }
+}
+
+class RecipeContainer extends React.Component {
+    render() {
+        let recipes = [];
+        for (let i = 0; i < data.recipes.length; i++) {
+            recipes.push(<Recipe
+                src={require('../public/img' + data.recipes[i].img)}
+                recipeTitle={data.recipes[i].name}
+                recipeDescription={data.recipes[i].description}
+            />)
+        }
+
+        return (
+            <div className="recipe-container">
+                {recipes}
+            </div>
+        );
+    }
+
+    componentWillMount() {
+        // document.body.style.backgroundImage = './background.jpeg';
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RecipeContainer/>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
